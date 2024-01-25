c227d4676ed05ff8429f78e1e70724b971043499
vittali <peter.vittali@openfokus.org>
2024-01-25 23:55:18 +0100

    tips

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
-rsync -a --verbose --perms --times  --prune-empty-dirs  ../vittali.ch/build/ .
+rsync -a --verbose --perms --times  --prune-empty-dirs ../vittali.ch/build/ .
+rsync -a --verbose --perms --times  --prune-empty-dirs ../TiPs_1/build/ TiPs_1
